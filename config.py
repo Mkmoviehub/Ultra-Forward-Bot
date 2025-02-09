ff4c559d16deebb40c968ee30251987b2be05f5a
Mkmoviehub <kondapallishiva143@gmail.com>
2025-02-09 20:28:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://ipsappdevelopers:ipsappdevelopers@cluster0.j5q11.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "mongodb")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "ipsappdevelopers")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '472034722').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002259804858'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
